e6fa89caf085fa248f7a638afcacbd116773a249
William JACQUEMET <william.jacquemet@protonmail.com>
2019-04-29 12:05:56 +0000

    adding terraform and packer file for the cloud-courses project

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "scaleway" {
+  region = "ams1"
+}
+
+data "scaleway_image" "terraformwilliam" {
+  architecture = "x86_64"
+  name         = "Ubuntu Bionic"
+}
+
+resource "scaleway_server" "terraformwilliam" {
+  name  = "terraformwilliam"
+  image = "${data.scaleway_image.terraformwilliam.id}"
+  type  = "START1-S"
+}
+resource "scaleway_ip" "ip" {
+  server = "${scaleway_server.terraformwilliam.id}"
+}
+
+
+resource "scaleway_volume" "terraformwilliam" {
+  name       = "terraformwilliamvolume"
+  size_in_gb = 50
+  type       = "l_ssd"
+}
+
+resource "scaleway_security_group" "http" {
+  name        = "http"
+  description = "allow HTTP and HTTPS traffic"
+}
+
+resource "scaleway_security_group_rule" "http_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 80
+}
+
+resource "scaleway_security_group_rule" "https_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 443
+}
+
